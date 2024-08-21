f05d224750baff9f5244d61945a49d8b0fc71a1a
JimuelG <jimuelgaas@gmail.com>
2024-08-21 17:41:46 +0800

    kct_app v1

---
+++ android/app/src/main/kotlin/com/example/kct/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kct
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/Core/BottomNavBar.dart
@@ -0,0 +1,62 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:kct/Core/screens/home_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final appScreen = [
+    const HomeScreen(),
+    const Text("Menu"),
+    const Text("Settings"),
+    const Text("Setting"),
+  ];
+
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: appScreen[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          selectedItemColor: Colors.blueGrey,
+          unselectedItemColor: const Color(0xFF526400),
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_home_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_home_filled),
+                label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_list_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_list_filled),
+                label: "Menu"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_settings_dev_regular),
+                activeIcon:
+                    Icon(FluentSystemIcons.ic_fluent_settings_dev_filled),
+                label: "Settings"),
+            BottomNavigationBarItem(
+                icon: Icon(FluentSystemIcons.ic_fluent_settings_dev_regular),
+                activeIcon:
+                Icon(FluentSystemIcons.ic_fluent_settings_dev_filled),
+                label: "Setting"),
+          ]),
+    );
+  }
+}
