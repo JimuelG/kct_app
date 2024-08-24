38c3da5d08fb4c38fe7477e762bd1fa25f020538
JimuelG <jimuelgaas@gmail.com>
2024-08-25 01:04:05 +0800

    kct_app v1.4

--- lib/Core/BottomNavBar.dart
+++ lib/Core/BottomNavBar.dart
@@ -2,6 +2,7 @@ import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:kct/screens/home/home_screen.dart';
 import 'package:kct/screens/search/search_screen.dart';
+import 'package:kct/screens/ticket/ticket_screen.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -14,7 +15,7 @@ class _BottomNavBarState extends State<BottomNavBar> {
   final appScreen = [
     const HomeScreen(),
     const SearchScreen(),
-    const Text("Menu"),
+    const TicketScreen(),
     const Text("Settings"),
   ];
 
@@ -47,9 +48,9 @@ class _BottomNavBarState extends State<BottomNavBar> {
                 activeIcon: Icon(FluentSystemIcons.ic_fluent_search_filled),
                 label: "Search"),
             BottomNavigationBarItem(
-                icon: Icon(FluentSystemIcons.ic_fluent_list_regular),
-                activeIcon: Icon(FluentSystemIcons.ic_fluent_list_filled),
-                label: "Menu"),
+                icon: Icon(FluentSystemIcons.ic_fluent_ticket_regular),
+                activeIcon: Icon(FluentSystemIcons.ic_fluent_ticket_filled),
+                label: "Ticket"),
             BottomNavigationBarItem(
                 icon: Icon(FluentSystemIcons.ic_fluent_settings_dev_regular),
                 activeIcon:
--- lib/Core/res/media.dart
+++ lib/Core/res/media.dart
@@ -1,4 +1,5 @@
 class AppMedia{
   static const _baseImage = "assets/images";
   static const logo = '$_baseImage/aveiro_logo.png';
+  static const planeSit = "$_baseImage/plane_sit.jpg";
 }
